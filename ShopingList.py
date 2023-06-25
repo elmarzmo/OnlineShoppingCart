c85baf5bdc65966f10bb165783916942c2275534
Mohamed Elmarzougui <Mohamed1elmarzougui@gmail.com>
2023-06-25 11:28:06 -0500

    Update the code

--- ShopingList.py
+++ ShopingList.py
@@ -1,20 +1,29 @@
-import datetime
-num =0
-total_price =0.0
-name = input("Please enter your name")
-print("{}'s Shopping Cart-{}".format( name, datetime.date.today()))
-while True:
-    items_name = input("Please enter the item name: ")
-    items_price = float(input("Please enter the price: "))
-    items_quantity = int(input("Please enter the quantity: "))
+class ItemToPurchase:
+    def __init__(self, item_name="none", item_price= 0, item_quantity=0):
+        self.item_name = item_name
+        self.item_price = item_price
+        self.item_quantity = item_quantity
+    def print_item_cost(self):
+        total_cost = self.item_quantity * self.item_price
+        print(f"{self.item_name} {self.item_quantity} @ ${self.item_price} = ${total_cost}")
+        return total_cost
+def main():
+    print('Item 1')
+    item_name = input("Enter the item name:")
+    item_price = float(input("\nEnter the item price:"))
+    item_quantity = int(input('\nEnter the item quantity:'))
+    item1 =ItemToPurchase(item_name, item_price, item_quantity)
 
-    total_cost = items_price * items_quantity
-    num=num+items_quantity
-    total_price = total_price + total_cost
-    print('TOTAL COST:\n{} {} @ ${} = ${}'.format(items_name, items_quantity, items_price, total_cost))
+    print('\nItem 2')
+    item_name = input("Enter the item name:")
+    item_price = float(input("\nEnter the item price:"))
+    item_quantity = int(input('\nEnter the item quantity:'))
+    item2 =ItemToPurchase(item_name, item_price, item_quantity)
 
-    choice = input("Do you want to enter another item? (yes/no): ")
-    if choice.lower() != "yes":
-        break
-print( "number of Items:", num)
-print("Total:", total_price, '$')
+    print('\nTOTAL COST: ')
+    item1.print_item_cost()
+    item2.print_item_cost()
+    total = item1.print_item_cost() + item2.print_item_cost()
+    print('Total: $',total)
+if __name__== "__main__" :
+    main()
